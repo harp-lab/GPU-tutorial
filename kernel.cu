a9bf30a130a8626091dd6accc01af9576a9f6af9
sami141215 <54345294+sami141215@users.noreply.github.com>
2020-04-16 13:21:30 -0400

    Checking Recursion
    It doesn't work. I was trying to see if a general kernel can be recursive. It turns out that to call a kernel recursively, we need to define a kernel using device keyword instead of global and a special gpu of FERMI architecture is needed to do that.

--- kernel.cu
+++ kernel.cu
@@ -1,169 +1,127 @@
+
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
+
 #include <stdio.h>
-#include <time.h>
-#include <chrono> 
-#include <iostream> 
-using namespace std;
-using namespace std::chrono;
 
-void print(int* array, unsigned int size, string name);
+cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
 
-__global__ void power(int *c, const int *a, const int *helper)
+__global__ void addKernel(int *c, const int *a, int *b)
 {
-    int blockId = blockIdx.x;
-    int threadId = threadIdx.x;
-    int numberOfThread = helper[0];
-    int seqPart = helper[1];
-    int st = seqPart * (blockId*numberOfThread + threadId);
-    
-    
-    int i = 0;
-    for (;i < seqPart;i++) {
-        c[st+i] = (a[st+i] * a[st+i]);
-        printf("A[%d]: %d & C[%d]: %d\n", (st + i),a[st + i],(st + i),c[st + i]);
+    if (b[0] > 0) {
+        printf("Hi %d\n", b[0]);
+        b[0]= b[0]-1;
     }
+    //int i = threadIdx.x;
+    //c[i] = a[i] + b[i];
+    //addKernel(c, a, b);
+    addKernel << <1, 1 >> > (c, a, b);
 }
 
-cudaError_t squareWithCuda(int* c, int* a, int size, int blk, int thrd)
+int main()
 {
-    print(a, size, "A at the beginning of the CUDA method");
-    int seqPart = size / (blk * thrd);
-    int* helper = new int(2);
-    helper[0] = thrd;
-    helper[1] = seqPart;
-    int *dev_helper = 0;
+    const int arraySize = 5;
+    const int a[arraySize] = { 1, 2, 3, 4, 5 };
+    const int b[arraySize] = { 10, 20, 30, 40, 50 };
+    int c[arraySize] = { 0 };
+
+    // Add vectors in parallel.
+    cudaError_t cudaStatus = addWithCuda(c, a, b, arraySize);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "addWithCuda failed!");
+        return 1;
+    }
+
+    printf("{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n",
+        c[0], c[1], c[2], c[3], c[4]);
+
+    // cudaDeviceReset must be called before exiting in order for profiling and
+    // tracing tools such as Nsight and Visual Profiler to show complete traces.
+    cudaStatus = cudaDeviceReset();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaDeviceReset failed!");
+        return 1;
+    }
+
+    return 0;
+}
+
+// Helper function for using CUDA to add vectors in parallel.
+cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size)
+{
     int *dev_a = 0;
+    int *dev_b = 0;
     int *dev_c = 0;
     cudaError_t cudaStatus;
 
-    cudaStatus = cudaMalloc((void**)&dev_helper, 2 * sizeof(int));
+    // Choose which GPU to run on, change this on a multi-GPU system.
+    cudaStatus = cudaSetDevice(0);
     if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed while allocating spaces for the helper!");
+        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
         goto Error;
     }
 
+    // Allocate GPU buffers for three vectors (two input, one output)    .
     cudaStatus = cudaMalloc((void**)&dev_c, size * sizeof(int));
     if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed while allocating spaces for output!");
+        fprintf(stderr, "cudaMalloc failed!");
         goto Error;
     }
 
     cudaStatus = cudaMalloc((void**)&dev_a, size * sizeof(int));
     if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed while allocating spaces for input!");
+        fprintf(stderr, "cudaMalloc failed!");
         goto Error;
     }
-    print(a, size, "A just before the kernel call");
-    cudaStatus = cudaMemcpy(dev_helper, helper, 2 * sizeof(int), cudaMemcpyHostToDevice);
+
+    cudaStatus = cudaMalloc((void**)&dev_b, size * sizeof(int));
     if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed while transfering values of the helper!");
+        fprintf(stderr, "cudaMalloc failed!");
         goto Error;
     }
-    
+
+    // Copy input vectors from host memory to GPU buffers.
     cudaStatus = cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
     if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed while transfering values of a (input)!");
+        fprintf(stderr, "cudaMemcpy failed!");
+        goto Error;
+    }
+
+    cudaStatus = cudaMemcpy(dev_b, b, size * sizeof(int), cudaMemcpyHostToDevice);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMemcpy failed!");
+        goto Error;
+    }
+
+    // Launch a kernel on the GPU with one thread for each element.
+    addKernel<<<1, size>>>(dev_c, dev_a, dev_b);
+
+    // Check for any errors launching the kernel
+    cudaStatus = cudaGetLastError();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
         goto Error;
     }
     
-    // Launch a kernel
-    power <<< blk, thrd >> > (dev_c, dev_a, dev_helper);
-    
+    // cudaDeviceSynchronize waits for the kernel to finish, and returns
+    // any errors encountered during the launch.
+    cudaStatus = cudaDeviceSynchronize();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
+        goto Error;
+    }
 
-        cudaStatus = cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
+    // Copy output vector from GPU buffer to host memory.
+    cudaStatus = cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaMemcpy failed!");
         goto Error;
     }
 
 Error:
-    cudaFree(dev_helper);
     cudaFree(dev_c);
     cudaFree(dev_a);
-    //cudaFree(dev_b);
+    cudaFree(dev_b);
     
     return cudaStatus;
-}
-
-void squareWithoutCuda(int* d, int* a, int size) {
-    for (int i = 0;i < size;i++) {
-        d[i] = a[i] * a[i];
-        //a[i] * a[i] >> d[i];//[i] = a[i] * a[i];//
-    }
-}
-
-int main()
-{
-    int numOfBlocks = 1;
-    int numOfThreads = 1;
-    bool checkFinal = true;
-    int arraySize = 1024;
-    //long* a = new long(arraySize);
-    int* c = new int(arraySize);
-    int* d = new int(arraySize);
-    int* a = new int(arraySize);
-    //int a[arraySize];
-    //int c[arraySize] = { 0 };
-    //int d[arraySize] = { 0 };
-    //for (int itr = 1;itr <= 50;itr++) {        
-        
-        for (int i = 0; i < arraySize; i++) {
-            int randomNumber = 1 + 100 * (rand() / (RAND_MAX + 1.0));
-            a[i] = randomNumber;
-            //randomNumber >> a[i];//a[i] = rand();//
-        }
-        print(a, arraySize, "Array a");
-        auto start1 = high_resolution_clock::now();
-        squareWithoutCuda(d, a, arraySize);
-        auto stop1 = high_resolution_clock::now();
-        print(d, arraySize, "Seq");
-        auto start2 = high_resolution_clock::now();
-        squareWithCuda(c, a, arraySize, numOfBlocks, numOfThreads);
-        auto stop2 = high_resolution_clock::now();
-        print(c, arraySize, "Par");
-        
-        
-
-        auto duration1 = duration_cast<microseconds>(stop1 - start1);
-        
-        auto duration2 = duration_cast<microseconds>(stop2 - start2);
-        cout << numOfBlocks << "," << numOfThreads << "," << duration1.count() << "," << duration2.count() << endl;
-
-        bool check = true;
-        int count = 0;
-        for (int i = 0;i < arraySize;i++) {
-            if (c[i] != d[i]) {
-                check = false;
-                break;
-            }
-            else {
-                count++;
-            }
-
-        }
-        delete[] a;
-        delete[] c;
-        delete[] d;
-        if (!check) {
-            checkFinal = !checkFinal;
-        }
-        
-    //}
-    if (checkFinal) {
-        printf("%s\n==================\n", "PASS");
-    }
-    else {
-        printf("%s\n==================\n", "FAIL");
-    }
-
-    return 0;
-}
-void print(int* array, unsigned int size, string name) {
-    printf("%s","Array printing starts: ");
-    cout << name << endl;
-    for (int i = 0;i < size-1;i++) {
-        printf("%d; ",array[i]);
-    }
-    printf("%d\n%s\n", array[size-1],"Array printing ends.");
 }
